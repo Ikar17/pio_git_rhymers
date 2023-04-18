de6d4b21fe714ec49a0da397264794bcdf2b42f1
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-04-18 23:36:19 +0200

    6.2 Poprawione stałe

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,10 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static int[] INTS = new int[12];
-    private final int[] numbers = INTS;
+    public static final int SIZE = 12;
+    public static final int SIZE_FULL = 11;
+    public static int[] INTS = SIZE;
+    private final int[] numbers = new int[SIZE];
 
-    public int total = -1;
+    public int total = ERROR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -13,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == SIZE_FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR;
         return numbers[total--];
     }
 
