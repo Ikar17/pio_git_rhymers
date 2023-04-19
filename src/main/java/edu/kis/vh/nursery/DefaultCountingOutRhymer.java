90bd2a8fb9bfa09c994ac8d98a227765e6943106
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-04-19 11:06:31 +0200

    6.8 Poprawiona zależność między 11, a 12

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     private static final int SIZE_TABLE = 12;
-    private static final int SIZE_FULL = 11;
+    private static final int SIZE_FULL = SIZE_TABLE-1;
     private static final int EMPTY = -1;
     private static final int VALUE_START =-1;
     private final int[] numbers = new int[SIZE_TABLE];
