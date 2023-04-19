5c2f1323d494a434531404dcc67de8fc58ad0c18
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-04-19 10:49:45 +0200

    6.6 Poprawionie nazwy ERROR na EMPTY

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,10 +4,10 @@ public class DefaultCountingOutRhymer {
 
     private static final int SIZE_TABLE = 12;
     private static final int SIZE_FULL = 11;
-    private static final int ERROR = -1;
+    private static final int EMPTY = -1;
     private final int[] numbers = new int[SIZE_TABLE];
 
-    public int total = ERROR;
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -15,7 +15,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == ERROR;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
@@ -24,13 +24,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return ERROR;
+            return EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return ERROR;
+            return EMPTY;
         return numbers[total--];
     }
 
